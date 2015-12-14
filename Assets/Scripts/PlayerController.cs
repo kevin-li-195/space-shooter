dcc407595d91fe11e6df7f593004bdc082353cc6
Kevin Li <kevin.li195@hotmail.com>
2015-12-14 18:53:21 -0500

    Complete general shooting script, basic bolt animation, game boundary for object destruction, main ortho camera, player movement.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+
+public class Constraint {
+	public float xMin, xMax, zMin, zMax, speed, tilt;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public Constraint boundary;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate = 0.3f;
+    private float nextFire = 0.0f;
+
+    // Called on every frame.
+    void Update() {
+        if (Input.GetButton("Fire1") && Time.time > nextFire) {
+            GameObject clone = Instantiate(shot, shotSpawn.position, shotSpawn.rotation) as GameObject;
+            nextFire = Time.time + fireRate;
+        }
+    }
+
+	// Called before each physics step.
+	void FixedUpdate() {
+		float mvHori = Input.GetAxis ("Horizontal");
+		float mvVert = Input.GetAxis ("Vertical");
+		Rigidbody rgbody = GetComponent<Rigidbody> ();
+		Vector3 clamp = new Vector3 (
+			Mathf.Clamp (rgbody.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp (rgbody.position.z, boundary.zMin, boundary.zMax)
+		);
+		Vector3 mvmt = new Vector3 (mvHori, 0.0f, mvVert);
+		mvmt *= boundary.speed;
+		Vector3 tiltV = new Vector3 (0.0f, 0.0f, mvHori * -boundary.tilt);
+
+		rgbody.velocity = mvmt;
+		rgbody.position = clamp;
+		rgbody.rotation = Quaternion.Euler(tiltV);
+	}
+}
