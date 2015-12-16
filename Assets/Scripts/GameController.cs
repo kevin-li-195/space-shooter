7395aca89862e6dba2cedfc4e9e96b4422c231f1
Kevin Li <kevin.li195@hotmail.com>
2015-12-16 14:36:54 -0500

    Fix restart problem where could only restart once and loop BGM

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
@@ -59,7 +60,7 @@ public class GameController : MonoBehaviour {
     void Update() {
         if (restart) {
             if (Input.GetKeyDown(KeyCode.R)) {
-                Application.LoadLevel(Application.loadedLevel);
+                SceneManager.LoadScene("main");
             }
         }
     }
